567c37b4355fb2b329b37f09bf7d47190f0fb141
VChapdelaineT <96508889+VChapdelaineT@users.noreply.github.com>
2022-04-12 21:38:21 -0400

    Major efficiency update (requieres sorted files)

--- VCF_comparison.sh
+++ VCF_comparison.sh
@@ -2,54 +2,46 @@
 ### Objective : Comparing VCFs file (Target to Reference)
 ### All target variant are compared to the Reference (values, samples)
 # Necessity : Equal versions (issue tracted, VCT)
-# Efficiency improvement welcomed
+# Requieres sorted VCF
 
 
 import pysam
 import argparse
 import warnings
 
-
 argparser = argparse.ArgumentParser(description = 'Comparison of 2 VCFs')
-argparser.add_argument('-R', '--Ref-vcf', metavar = 'file', dest = 'Rvcf', required = True, help = 'Reference Variant File, Origin VCFs')
-argparser.add_argument('-T', '--Test-vcf', metavar = 'file', dest = 'Cvcf', required = True, help = 'File to compare')
+argparser.add_argument('-R', '--Ref-vcf', metavar = 'file', dest = 'Rvcf', required = True, help = 'Reference Variant File, Original sorted VCFs')
+argparser.add_argument('-T', '--Test-vcf', metavar = 'file', dest = 'Cvcf', required = True, help = 'File to compare (sorted)')
 
-#to test if samples are equivalent
 def test_samples(R_VCF,C_VCF):
 	if set(R_VCF.header.samples) == set(C_VCF.header.samples):
 		print("OK .................... Sample are Equivalent")
 		return set(C_VCF.header.formats.keys()).intersection(R_VCF.header.formats.keys())
 	elif '_' in R_VCF.header.samples[1] and '_' not in C_VCF.header.samples[1]:
-		warnings.warn('Delimiter might cause problem') # if simply a case of plink's double-id no problem should arise
+		warnings.warn('Delimiter might cause problem')
 		return True
 	elif '_' in C_VCF.header.samples[1] and '_' not in R_VCF.header.samples[1]:
-		warnings.warn("Delimiter might cause problem") # if simply a case of plink's double-id no problem should arise
+		warnings.warn("Delimiter might cause problem")
 		return True
 	else :
 		print('failed ........... Samples are not Equivalent')
 		return False
 
-def test_format(record_C,record_R):
-	formats = set(record_R.format.keys()).intersection(record_C.format.keys())
-	if not formats:
-		print('failed ........... Reference format does not include test format')
-	else :
-		return formats
-
-
 def test_Vequal(C_VCF,R_VCF):
+	REF=R_VCF.fetch()
+	current=next(REF)
 	for rec_C in C_VCF.fetch():
 		equality=False
-    		REF=R_VCF.fetch("chr" + rec_C.contig,rec_C.pos-1,rec_C.pos)       
-		for rec_REF in REF:
-			if rec_REF.alts == rec_C.alts and rec_REF.ref ==rec_C.ref :
-				equality = equality or test_values_Vequal(rec_C,rec_REF)
-			if equality:
-				break
-		if not equality: ##Cases where no record in Reference, Cases where values are unequal, Cases where no format are overlapping
+		while(current.pos<=rec_C.pos):
+			if current.alts == rec_C.alts and current.ref ==rec_C.ref and test_values_Vequal(rec_C,current):
+				equality=True
+				break      
+			else:
+				current=next(REF)
+		if not equality:
 			print("problem Position ", rec_C.pos)
 			break
-	print("Test Terminated")		
+	print("Test Terminated")			
 
 def test_values_Vequal(record_C,record_R):
 	returned=True
@@ -61,7 +53,12 @@ def test_values_Vequal(record_C,record_R):
 				return returned
 	return returned
 
-
+def test_format(record_C,record_R):
+	formats = set(record_R.format.keys()).intersection(record_C.format.keys())
+	if not formats:
+		print('failed ........... Reference format does not include test format')
+	else :
+		return formats
 
 if __name__ == "__main__":
 	args = argparser.parse_args()
@@ -69,5 +66,3 @@ if __name__ == "__main__":
 	R = pysam.VariantFile(args.Rvcf,'r')
 	if test_samples(C,R):
 		test_Vequal(C,R)
-
-
